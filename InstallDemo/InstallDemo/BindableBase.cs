006eba5479dcc9cdc635abe3d16f6f84e6ecf79d
Brandon Best <brandonbest94@live.ca>
2017-01-16 19:55:34 -0700

    Implementation of BindableBase class that implements INotifyPropertyChanged in order to subclass BindableBase in modles or view models to make them ready for nontrivial binding.  This allows our data-bound UI to refresh and reflect changes to the data model in real time.

---
+++ InstallDemo/InstallDemo/BindableBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace InstallDemo
+{
+    public abstract class BindableBase: INotifyPropertyChanged 
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        var handler = PropertyChanged;
+        if (handler != null)
+        {
+            handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    }
+}
+
